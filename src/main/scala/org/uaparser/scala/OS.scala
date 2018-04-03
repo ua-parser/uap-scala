4263c892ee3e249d10bfb35291c27c41f3ec32af
Travis Brown <travisrobertbrown@gmail.com>
2018-04-03 04:58:58 -0500

    Fix major OS version parsing to match uap-core #286

--- src/main/scala/org/uaparser/scala/OS.scala
+++ src/main/scala/org/uaparser/scala/OS.scala
@@ -1,6 +1,6 @@
 package org.uaparser.scala
 
-import java.util.regex.Pattern
+import java.util.regex.{ Matcher, Pattern }
 import MatcherOps._
 
 case class OS(family: String, major: Option[String] = None, minor: Option[String] = None, patch: Option[String] = None,
@@ -10,6 +10,13 @@ object OS {
   private[scala] def fromMap(m: Map[String, String]) = m.get("family").map { family =>
     OS(family, m.get("major"), m.get("minor"), m.get("patch"), m.get("patch_minor"))
   }
+
+  private[this] val quotedBack1: Pattern = Pattern.compile(s"(${Pattern.quote("$1")})")
+
+  private[this] def replacementBack1(matcher: Matcher)(replacement: String): String =
+    if (matcher.groupCount() >= 1) {
+      quotedBack1.matcher(replacement).replaceAll(matcher.group(1))
+    } else replacement
 
   private[scala] case class OSPattern(
     pattern: Pattern,
@@ -22,16 +29,16 @@ object OS {
     def process(agent: String): Option[OS] = {
       val matcher = pattern.matcher(agent)
       if (!matcher.find()) None else {
-        osReplacement.map { replacement =>
-          if (matcher.groupCount() >= 1)
-            Pattern.compile(s"(${Pattern.quote("$1")})").matcher(replacement).replaceAll(matcher.group(1))
-          else replacement
-        }.orElse(matcher.groupAt(1)).map { family =>
-          val major = v1Replacement.orElse(matcher.groupAt(2))
-          val minor = v2Replacement.orElse(matcher.groupAt(3))
-          val patch = v3Replacement.orElse(matcher.groupAt(4))
-          val patchMinor = v4Replacement.orElse(matcher.groupAt(5))
-          OS(family, major, minor, patch, patchMinor)
+        osReplacement
+          .map(replacementBack1(matcher))
+          .orElse(matcher.groupAt(1)).map { family =>
+            val major = v1Replacement
+              .map(replacementBack1(matcher))
+              .orElse(matcher.groupAt(2))
+            val minor = v2Replacement.orElse(matcher.groupAt(3))
+            val patch = v3Replacement.orElse(matcher.groupAt(4))
+            val patchMinor = v4Replacement.orElse(matcher.groupAt(5))
+            OS(family, major, minor, patch, patchMinor)
         }
       }
     }
