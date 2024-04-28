29395a58f9b326c0ddff145cd67b7616ec883a5d
André Cardoso <thyandrecardoso@gmail.com>
2024-04-28 13:12:56 +0100

    Avoid some compilation warnings

---
+++ src/main/scala-3/org/uaparser/scala/YamlUtil.scala
@@ -0,0 +1,24 @@
+package org.uaparser.scala
+
+import org.yaml.snakeyaml.{LoaderOptions, Yaml}
+import org.yaml.snakeyaml.constructor.SafeConstructor
+import java.io.InputStream
+import scala.jdk.CollectionConverters._
+import scala.util.Try
+import org.yaml.snakeyaml.{LoaderOptions, Yaml}
+import org.yaml.snakeyaml.constructor.SafeConstructor
+import java.util.{List => JList, Map => JMap}
+import scala.collection.MapView
+
+private[scala] object YamlUtil {
+  def loadYamlAsMap(yamlStream: InputStream, loader: Yaml): Map[String, List[Map[String, String]]] = {
+    val javaConfig = loader.load[JMap[String, JList[JMap[String, String]]]](yamlStream)
+    javaConfig.asScala.map { case (k, v) =>
+      k -> v.asScala.map(_.asScala.filter{case (_, v) => v != null}.toMap).toList
+    }.toMap
+  }
+
+  def loadYamlAsMap(yamlStream: InputStream): Map[String, List[Map[String, String]]] = {
+    loadYamlAsMap(yamlStream, new Yaml(new SafeConstructor(new LoaderOptions))  )
+  }
+}
--- src/main/scala/org/uaparser/scala/Parser.scala
+++ src/main/scala/org/uaparser/scala/Parser.scala
@@ -1,13 +1,9 @@
 package org.uaparser.scala
 
 import java.io.InputStream
-import java.util.{List => JList, Map => JMap}
 import org.uaparser.scala.Device.DeviceParser
 import org.uaparser.scala.OS.OSParser
 import org.uaparser.scala.UserAgent.UserAgentParser
-import org.yaml.snakeyaml.{LoaderOptions, Yaml}
-import org.yaml.snakeyaml.constructor.SafeConstructor
-import scala.collection.JavaConverters._
 import scala.util.Try
 
 case class Parser(userAgentParser: UserAgentParser, osParser: OSParser, deviceParser: DeviceParser)
@@ -18,11 +14,7 @@ case class Parser(userAgentParser: UserAgentParser, osParser: OSParser, devicePa
 
 object Parser {
   def fromInputStream(source: InputStream): Try[Parser] = Try {
-    val yaml = new Yaml(new SafeConstructor(new LoaderOptions))
-    val javaConfig = yaml.load[JMap[String, JList[JMap[String, String]]]](source)
-    val config = javaConfig.asScala.toMap.mapValues(_.asScala.toList.map(_.asScala.toMap.filterNot {
-      case (_ , value) => value eq null
-    }))
+    val config = YamlUtil.loadYamlAsMap(source)
     val userAgentParser = UserAgentParser.fromList(config.getOrElse("user_agent_parsers", Nil))
     val osParser = OSParser.fromList(config.getOrElse("os_parsers", Nil))
     val deviceParser = DeviceParser.fromList(config.getOrElse("device_parsers", Nil))
