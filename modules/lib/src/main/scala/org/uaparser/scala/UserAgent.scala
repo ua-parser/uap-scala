bbe344769bdec5109cb7a176b37183023369ff7d
André Cardoso <thyandrecardoso@gmail.com>
2025-09-12 15:01:13 +0100

    Extract *pattern classes to their own files

--- modules/lib/src/main/scala/org/uaparser/scala/UserAgent.scala
+++ modules/lib/src/main/scala/org/uaparser/scala/UserAgent.scala
@@ -1,9 +1,5 @@
 package org.uaparser.scala
 
-import java.util.regex.{Matcher, Pattern}
-
-import org.uaparser.scala.MatcherOps.*
-
 case class UserAgent(
     family: String,
     major: Option[String] = None,
@@ -14,44 +10,6 @@ case class UserAgent(
 object UserAgent {
   private[scala] def fromMap(m: Map[String, String]) = m.get("family").map { family =>
     UserAgent(family, m.get("major"), m.get("minor"), m.get("patch"))
-  }
-
-  private[scala] case class UserAgentPattern(
-      pattern: Pattern,
-      familyReplacement: Option[String],
-      v1Replacement: Option[String],
-      v2Replacement: Option[String],
-      v3Replacement: Option[String]
-  ) {
-    def process(agent: String): Option[UserAgent] = {
-      val matcher = pattern.matcher(agent)
-      if (!matcher.find()) return None
-      familyReplacement
-        .map { replacement =>
-          if (replacement.contains("$1") && matcher.groupCount() >= 1) {
-            replacement.replaceFirst("\\$1", Matcher.quoteReplacement(matcher.group(1)))
-          } else replacement
-        }
-        .orElse(matcher.groupAt(1))
-        .map { family =>
-          val major = v1Replacement.orElse(matcher.groupAt(2)).filter(_.nonEmpty)
-          val minor = v2Replacement.orElse(matcher.groupAt(3)).filter(_.nonEmpty)
-          val patch = v3Replacement.orElse(matcher.groupAt(4)).filter(_.nonEmpty)
-          UserAgent(family, major, minor, patch)
-        }
-    }
-  }
-
-  private object UserAgentPattern {
-    def fromMap(config: Map[String, String]): Option[UserAgentPattern] = config.get("regex").map { r =>
-      UserAgentPattern(
-        Pattern.compile(r),
-        config.get("family_replacement"),
-        config.get("v1_replacement"),
-        config.get("v2_replacement"),
-        config.get("v3_replacement")
-      )
-    }
   }
 
   case class UserAgentParser(patterns: List[UserAgentPattern]) {
